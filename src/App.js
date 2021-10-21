9e0b128e4fe474cbc4c3380ed68639291ceca4c9
rugieri <rugieri.ronaldo@gmail.com>
2021-10-20 23:12:18 -0300

    add languages to translator

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Languages from './components/languages';
 import Translate from './components/translate';
 
 export default function App() {
-  const [language, setlanguage] = useState('ru');
+  const [language, setLanguage] = useState('ru');
   const [text, setText] = useState('');
 
   return (
     <div>
       <Field label="Enter English" onChange={setText} value={text} />
-      <Languages />
+      <Languages language={language} onlanguageChange={setLanguage} />
       <hr />
       <Translate />
     </div>
